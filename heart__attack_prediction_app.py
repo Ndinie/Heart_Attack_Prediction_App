28fc528afd605141fff9d0ac1ba56d9167a9d726
Ndinie <86551129+Ndinie@users.noreply.github.com>
2022-07-26 00:00:25 +0800

    Update heart_attack_[predict_app.py

--- heart__attack_prediction_app.py
+++ heart__attack_prediction_app.py
@@ -49,14 +49,14 @@ with col2:
         cp = st.selectbox('Chest Pain Type (0 = Typical Angina, 1 = Atypical Angina, 2 = Non-anginal Pain, 3 = Asymptomatic)',(0, 1, 2, 3))
         trtbps = st.slider("Resting Blood Pressure", 0, 200)
         chol = st.slider("Cholesterol Level", 100, 600)
-        fbs = st.selectbox("Fasting Blood Sugar (1 = True, 0 = False) ", (0, 1))
+        fbs = st.selectbox("Fasting Blood Sugar (0 = False, 1 = True) ", (0, 1))
         rest_ecg = st.selectbox("Resting Electrocardiographic Results (0 = Normal, 1 = ST-T wave normality, 2 = Left ventricular hypertrophy) ", (0, 1, 2))
         thalach = st.slider("Maximum Heart Rate", 0, 220)
         exng = st.selectbox("Exercise Induced Angina (1 = Yes, 0 = No)", (0, 1))
         oldpeak = st.slider("ST Depression Induced by Exercise Relative to rest",0.0, 7.0)
-        slp = st.selectbox('Slope', (0, 2))
-        caa = st.selectbox('Number of Major Vessels', (0, 3))
-        thall = st.selectbox('Thalium Stress Test result', (0, 2, 3))
+        slp = st.selectbox('Slope', (0, 1, 2))
+        caa = st.selectbox('Number of Major Vessels', (0, 1, 2, 3))
+        thall = st.selectbox('Thalium Stress Test result', (0, 1, 2, 3))
         
         row = [age, sex, cp, trtbps, chol, fbs, rest_ecg, thalach, exng, oldpeak, slp, caa, thall]
 
